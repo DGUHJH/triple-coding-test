87c3ec679da721adc617f5335a9ca8e4d8f1ce48
DGUHJH <ghkdwogud852@gmail.com>
2022-03-18 14:03:41 +0900

    Fix : CommonImage 컴포넌트 속성값 오류 수정

--- src/components/Image/Common/styled.ts
+++ src/components/Image/Common/styled.ts
@@ -2,12 +2,14 @@ import styled from 'styled-components';
 import { ImageType } from 'types/common';
 
 export const Root = styled.img<ImageType>`
-  width: ${(props) => props.width};
+  width: ${(props) => (props.width ? `${props.width}px` : `100%`)};
   height: ${(props) => props.height ?? 'auto'};
   margin-top: ${(props) => props.marginTop ?? 0}px;
   margin-bottom: ${(props) => props.marginBottom ?? 0}px;
   margin-left: ${(props) => props.marginLeft ?? 0}px;
   margin-right: ${(props) => props.marginRight ?? 0}px;
-  border-radius: ${(props) => props.borderRadius}px;
+  border-radius: ${(props) => props.borderRadius ?? 0}px;
   cursor: ${(props) => (props.onClick ? `pointer` : `default`)};
+  background-repeat: no-repeat;
+  background-size: 100% 100%;
 `;
