87d4db8800f6f3302469cab95b83c8357fc762c6
DGUHJH <ghkdwogud852@gmail.com>
2022-03-20 11:41:31 +0900

    Feat : MainOnceContainer 컴포넌트 추가

---
+++ src/containers/Once/index.tsx
@@ -0,0 +1,66 @@
+import Phone from 'components/Phone';
+import CommonTypography from 'components/Typography/Common';
+import * as Styled from './styled';
+
+const MainOnceContainer = () => {
+  return (
+    <Styled.Root>
+      <Styled.SectionContainer>
+        <CommonTypography
+          fontFamily="sans-serif"
+          fontSize={52}
+          color="#3a3a3a"
+          lineHeight="68px"
+          fontWeight={500}
+        >
+          모든 예약을
+        </CommonTypography>
+        <CommonTypography
+          fontFamily="sans-serif"
+          fontSize={52}
+          color="#3a3a3a"
+          lineHeight="68px"
+          fontWeight={500}
+        >
+          최저가로 한 번에!
+        </CommonTypography>
+        <CommonTypography
+          fontFamily="sans-serif"
+          fontSize={17}
+          color="#3a3a3a"
+          lineHeight="30px"
+          marginTop={25}
+        >
+          항공권부터 숙소, 투어·티켓까지 최저가로 예약.
+        </CommonTypography>
+        <CommonTypography
+          fontFamily="sans-serif"
+          fontSize={17}
+          color="#3a3a3a"
+          lineHeight="30px"
+        >
+          트리플 전용 특가는 보너스.
+        </CommonTypography>
+        <Styled.PhoneContainer>
+          <Phone
+            src="https://triple.guide/intro/images/img-07-screen-3@3x.png"
+            width={262}
+            height={570}
+          />
+          <Phone
+            src="https://triple.guide/intro/images/img_07_screen_1@3x.png"
+            width={262}
+            height={570}
+          />
+          <Phone
+            src="https://triple.guide/intro/images/img_07_screen_2@3x.png"
+            width={262}
+            height={570}
+          />
+        </Styled.PhoneContainer>
+      </Styled.SectionContainer>
+    </Styled.Root>
+  );
+};
+
+export default MainOnceContainer;
---
+++ src/containers/Once/styled.ts
@@ -0,0 +1,26 @@
+import { Container } from '@mui/material';
+import styled from 'styled-components';
+
+export const Root = styled.div`
+  width: 100%;
+  height: 1000px;
+  background: center center / cover rgb(250, 250, 250);
+  overflow: hidden;
+`;
+
+export const SectionContainer = styled(Container)`
+  &&& {
+    width: 1040px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding-top: 150px;
+    box-sizing: border-box;
+  }
+`;
+
+export const PhoneContainer = styled.div`
+  display: flex;
+  gap: 20px;
+  margin-top: 100px;
+`;
