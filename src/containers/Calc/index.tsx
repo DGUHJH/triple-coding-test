8e478552361745593f4f143204c0b46f11738926
DGUHJH <ghkdwogud852@gmail.com>
2022-03-20 22:22:19 +0900

    Feat : MainCalcContainer 컴포넌트 이벤트 추가

--- src/containers/Calc/index.tsx
+++ src/containers/Calc/index.tsx
@@ -1,11 +1,53 @@
+import CommonImage from 'components/Image/Common';
 import CommonTypography from 'components/Typography/Common';
+import { useEffect, useState } from 'react';
 import * as Styled from './styled';
 
 type Props = {
   scrollY: number;
 };
 
+const label = ['날씨', '환율', '길찾기', '번역', '시차계산'];
+const imgSrc = [
+  'https://triple.guide/intro/images/img-06-card-1@3x.png?20190802',
+  'https://triple.guide/intro/images/img-06-card-2@3x.png?20190802',
+  'https://triple.guide/intro/images/img-06-card-3@3x.png?20190802',
+  'https://triple.guide/intro/images/img-06-card-4@3x.png?20190802',
+  'https://triple.guide/intro/images/img-06-card-5@3x.png?20190802',
+];
+
 const MainCalcContainer: React.FC<Props> = ({ scrollY }) => {
+  const [step, setStep] = useState<number>(0);
+  const [prevStep, setPrevStep] = useState<number>(0);
+  const [isChanging, setIsChanging] = useState<boolean>(false);
+  const [arrowType, setArrowType] = useState<'left' | 'right'>('left');
+
+  const onArrowButtonClick = (type: 'left' | 'right') => () => {
+    if (type === 'left') {
+      if (step !== 0) {
+        setPrevStep(step);
+        setStep((prev) => prev - 1);
+        setArrowType('left');
+      }
+    } else {
+      if (step !== 4) {
+        setPrevStep(step);
+        setStep((prev) => prev + 1);
+        setArrowType('right');
+      }
+    }
+  };
+
+  useEffect(() => {
+    setIsChanging(true);
+    const timer = setTimeout(() => {
+      setIsChanging(false);
+    }, 400);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [step]);
+
   return (
     <Styled.Root>
       <Styled.SectionContainer>
@@ -45,6 +87,64 @@ const MainCalcContainer: React.FC<Props> = ({ scrollY }) => {
         >
           이제 주머니 하나에 쏙 담아 언제든 쓱 꺼내보면 끝.
         </CommonTypography>
+        <Styled.CarouselContainer>
+          <Styled.CarouselHeaderContainer>
+            <Styled.CarouselHeaderPointBox left={step * 110} />
+            {label.map((value, index) => (
+              <Styled.CarouselHeaderItemWrapper
+                key={`carousel_header_item_${index}`}
+              >
+                <CommonTypography
+                  fontFamily="sans-serif"
+                  fontSize={17}
+                  lineHeight="30px"
+                  color={step === index ? 'rgb(54, 102, 255)' : '#fff'}
+                  fontWeight={step === index ? 700 : 400}
+                  onClick={() => setStep(index)}
+                  transition="all 300ms ease-out 200ms"
+                >
+                  {value}
+                </CommonTypography>
+              </Styled.CarouselHeaderItemWrapper>
+            ))}
+          </Styled.CarouselHeaderContainer>
+          <Styled.CarouselBodyContainer
+            arrowType={arrowType}
+            isChanging={isChanging}
+          >
+            <CommonImage
+              src={isChanging ? imgSrc[prevStep] : imgSrc[step]}
+              width={425}
+              height={300}
+              borderRadius={12}
+            />
+          </Styled.CarouselBodyContainer>
+          <Styled.CarouselArrowImgWrapper
+            top={230}
+            right={565}
+            isActive={step !== 0}
+          >
+            <CommonImage
+              src="https://triple.guide/intro/images/btn-06-arrow-r-over@3x.png"
+              width={50}
+              height={90}
+              rotate={180}
+              onClick={onArrowButtonClick('left')}
+            />
+          </Styled.CarouselArrowImgWrapper>
+          <Styled.CarouselArrowImgWrapper
+            top={270}
+            right={-80}
+            isActive={step !== 4}
+          >
+            <CommonImage
+              src="https://triple.guide/intro/images/btn-06-arrow-r-over@3x.png"
+              width={50}
+              height={90}
+              onClick={onArrowButtonClick('right')}
+            />
+          </Styled.CarouselArrowImgWrapper>
+        </Styled.CarouselContainer>
       </Styled.SectionContainer>
     </Styled.Root>
   );
--- src/containers/Calc/styled.ts
+++ src/containers/Calc/styled.ts
@@ -19,3 +19,80 @@ export const SectionContainer = styled(Container)`
     position: relative;
   }
 `;
+
+export const CarouselContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: relative;
+`;
+
+export const CarouselHeaderContainer = styled.div`
+  width: 550px;
+  display: flex;
+  position: relative;
+  margin-top: 60px;
+`;
+
+export const CarouselHeaderItemWrapper = styled.div`
+  width: 110px;
+  height: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  z-index: 2;
+`;
+
+type CarouselBodyContainerProps = {
+  isChanging: boolean;
+  arrowType: 'left' | 'right';
+};
+
+export const CarouselBodyContainer = styled.div<CarouselBodyContainerProps>`
+  height: 300px;
+  box-shadow: rgb(0 0 0 / 5%) 0px 10px 20px 0px;
+  position: relative;
+  margin-top: 50px;
+  ${(props) =>
+    props.isChanging &&
+    (props.arrowType === 'left'
+      ? `transform: translateX(50%); opacity: 0;`
+      : `transform: translateX(-50%); opacity: 0;`)}
+  ${(props) => !props.isChanging && `opacity: 1`}
+  transition: all 300ms ease-in-out 200ms;
+`;
+
+type CarouselArrowImgWrapperProps = {
+  top: number;
+  right: number;
+  isActive: boolean;
+};
+
+export const CarouselArrowImgWrapper = styled.div<CarouselArrowImgWrapperProps>`
+  position: absolute;
+  top: ${(props) => props.top}px;
+  right: ${(props) => props.right}px;
+  opacity: 0.3;
+  transition: all 300ms ease-in-out 0s;
+  &:hover {
+    ${(props) => props.isActive && `opacity: 1;`}
+  }
+`;
+
+type CarouselHeaderPointBoxProps = {
+  left: number;
+};
+
+export const CarouselHeaderPointBox = styled.div<CarouselHeaderPointBoxProps>`
+  width: 110px;
+  height: 50px;
+  background-color: rgb(255, 255, 255);
+  transition: all 300ms ease-in-out 200ms;
+  position: absolute;
+  box-shadow: rgb(0 0 0 / 5%) 0px 5px 10px 0px;
+  top: 0px;
+  left: ${(props) => props.left}px;
+  border-radius: 27px;
+  z-index: 1;
+`;
