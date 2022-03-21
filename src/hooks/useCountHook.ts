e36638df0c45f5252b350c76ed91fedc03d79704
DGUHJH <ghkdwogud852@gmail.com>
2022-03-21 10:01:36 +0900

    Feat : useCountHook 훅 기획에 맞게 수정

--- src/hooks/useCountHook.ts
+++ src/hooks/useCountHook.ts
@@ -1,20 +1,27 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react'
 
 const useCountHook = (start: number, end: number) => {
-  const [count, setCount] = useState(start);
+  const [count, setCount] = useState(start)
+  const slowCount = 15
 
   useEffect(() => {
+    const endCount = end - count
     if (count < end) {
-      const counter = setTimeout(() => {
-        setCount((prev) => prev + 1);
-      }, Math.floor(1000 / (start + end)));
+      const counter = setInterval(
+        () => {
+          setCount((prev) => prev + 1)
+        },
+        endCount <= slowCount
+          ? Math.round((2000 - end + slowCount) / slowCount)
+          : 1,
+      )
       return () => {
-        clearTimeout(counter);
-      };
+        clearInterval(counter)
+      }
     }
-  }, [count]);
+  }, [count])
 
-  return count;
-};
+  return count
+}
 
-export default useCountHook;
+export default useCountHook
